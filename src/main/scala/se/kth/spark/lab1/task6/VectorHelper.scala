b8e2e15736afc7a1f754ae0fd25d3814e36da4d2
victor-axelsson <victor_axelsson@hotmail.com>
2017-11-15 17:28:26 +0100

    Implemented the VectorHelper for task 6

--- src/main/scala/se/kth/spark/lab1/task6/VectorHelper.scala
+++ src/main/scala/se/kth/spark/lab1/task6/VectorHelper.scala
@@ -1,21 +1,51 @@
 package se.kth.spark.lab1.task6
 
-import org.apache.spark.ml.linalg.{Matrices, Vector, Vectors}
+import org.apache.spark.ml.linalg.{DenseVector, Matrices, Vector, Vectors}
 
 object VectorHelper {
   def dot(v1: Vector, v2: Vector): Double = {
-    ???
+    if(v1.size != v2.size){
+      throw new IllegalArgumentException("The dot product of two vector means that they have to be of same size")
+    }
+
+    var sum:Double = 0
+    for( i <- 0 to v1.size){
+      sum += v1(i) * v2(i)
+    }
+
+    sum
   }
 
   def dot(v: Vector, s: Double): Vector = {
-    ???
+    var sum:Double = 0
+
+    var newVals:Array[Double] = new Array(v.size)
+    for( i <- 0 to v.size){
+      newVals(i) = s * v(i)
+    }
+
+    new DenseVector(newVals)
   }
 
   def sum(v1: Vector, v2: Vector): Vector = {
-    ???
+    if(v1.size != v2.size){
+      throw new IllegalArgumentException("The sum of two vector means that they have to be of same size")
+    }
+
+    var newVals:Array[Double] = new Array(v1.size)
+    for( i <- 0 to v1.size){
+      newVals(i) = v1(i) + v2(i)
+    }
+
+    new DenseVector(newVals)
   }
 
   def fill(size: Int, fillVal: Double): Vector = {
-    ???
+    var newVals:Array[Double] = new Array(size)
+    for( i <- 0 to size){
+      newVals(i) = fillVal
+    }
+
+    new DenseVector(newVals)
   }
 }
