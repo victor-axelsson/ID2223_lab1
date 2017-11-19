06183004dcdb45e05430d0d839591c2a3662303d
victor-axelsson <victor_axelsson@hotmail.com>
2017-11-19 14:29:40 +0100

    SBT i not working

--- build.sbt
+++ build.sbt
@@ -14,7 +14,9 @@ libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.0.1"
 libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.0.1"
 libraryDependencies += "org.log4s" %% "log4s" % "1.3.3" % "provided"
 libraryDependencies += "se.kth.spark" %% "lab1_lib" % "1.0-SNAPSHOT"
+libraryDependencies += "org.apache.commons" % "commons-compress" % "1.11"
 
-mainClass in assembly := Some("se.kth.spark.lab1.task1.Main")
+
+mainClass in assembly := Some("se.kth.spark.lab1.task6.Main")
 
 assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)
--- src/main/scala/se/kth/spark/lab1/task6/Main.scala
+++ src/main/scala/se/kth/spark/lab1/task6/Main.scala
@@ -1,22 +1,55 @@
 package se.kth.spark.lab1.task6
 
 import org.apache.spark._
-import org.apache.spark.ml.{PipelineModel}
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
+import org.apache.spark.input.PortableDataStream
+
+import scala.util.Try
+import java.nio.charset._
+
+import org.apache.spark.rdd.RDD
+
 
 object Main {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("lab1").setMaster("local")
+    //val conf = new SparkConf().setAppName("lab1")
     val sc = new SparkContext(conf)
     val sqlContext = new SQLContext(sc)
 
     import sqlContext.implicits._
     import sqlContext._
 
-    val filePath = "src/main/resources/millionsong.txt"
+    //val filePath = "src/main/resources/millionsong.txt"
+    //val filePath = "hdfs:///Projects/labs/million_song/huge_dataset/*.tar.gz"
+    val filePath = "/home/victor/Desktop/dataset/fakeData/*.tar.gz"
 
-    val splits = sqlContext.read.text(filePath).toDF("row").randomSplit(Array(0.7, 0.3))
+    val reader = HDF5Factory.openForReading("farray.h5")
+    val mydata = reader.readFloatArray("mydata")
+    /*
+    val splits = sc.binaryFiles(filePath)
+      .flatMapValues(x => extractFiles(x).toOption)
+      .mapValues(_.map(decode()))
+      .flatMap((file: (String, Array[String])) => {
+        println()
+      }).map((r: String) => {
+        r.split("\n")
+      }).flatMap((r: Array[String]) => {
+        r
+      }).toDF("row").randomSplit(Array(0.7, 0.3))
+  */
 
+    val splits = sc.binaryFiles(filePath)
+      .flatMapValues(x => extractFiles(x).toOption)
+      .mapValues(_.map(decode())).take(4).foreach(r => {
+        println(r)
+      })
+
+    //val splits = sqlContext.read.text(filePath).toDF("row").randomSplit(Array(0.7, 0.3))
+  /*
     val obsDF = splits(0)
     val testDF = splits(1)
 
@@ -29,5 +62,34 @@ object Main {
     })
 
     lReg.transform(pipelineModel.transform(testDF).select("row", "features")).show(5)
+    */
   }
+
+
+  def extractFiles(ps: PortableDataStream, n: Int = 1024) = Try {
+    val tar = new TarArchiveInputStream(new GzipCompressorInputStream(ps.open))
+    Stream.continually(Option(tar.getNextTarEntry))
+      // Read until next exntry is null
+      .takeWhile(_.isDefined)
+      // flatten
+      .flatMap(x => x)
+      // Drop directories
+      .filter(!_.isDirectory)
+      .map(e => {
+        Stream.continually {
+          // Read n bytes
+          val buffer = Array.fill[Byte](n)(-1)
+          val i = tar.read(buffer, 0, n)
+          (i, buffer.take(i))}
+          // Take as long as we've read something
+          .takeWhile(_._1 > 0)
+          .map(_._2)
+          .flatten
+          .toArray})
+      .toArray
+  }
+
+  def decode(charset: Charset = StandardCharsets.UTF_8)(bytes: Array[Byte]) =
+    new String(bytes, StandardCharsets.UTF_8)
+
 }
