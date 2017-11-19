1124175dcddd425f3995301ee0cda65b8bbb27f7
RubbyRedson <gurnik2009@gmail.com>
2017-11-19 18:13:13 +0100

    Errors that too many files are open

--- src/main/scala/se/kth/spark/lab1/task6/Main.scala
+++ src/main/scala/se/kth/spark/lab1/task6/Main.scala
@@ -6,8 +6,10 @@ import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import ch.systemsx.cisd.hdf5._
 import java.io.File
 
+import org.apache.spark.ml.feature.{Normalizer, StandardScaler}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types._
+import se.kth.spark.lab1.Array2Vector
 
 import scala.util.Try
 
@@ -23,7 +25,7 @@ object Main {
     import sqlContext._
 
 
-    val files: Vector[String] = getPaths("/home/victor/Desktop/dataset/MillionSongSubset/data/")
+    val files: Vector[String] = getPaths("D:\\tmp\\millionsongsubset_full.tar\\millionsongsubset_full\\MillionSongSubset\\data\\")
     val h5PathRDD = sc.parallelize(files, 5)
 
     val songsRDD: RDD[Row] = h5PathRDD.map(open).flatMap(_.toOption)
@@ -91,11 +93,11 @@ object Main {
           r._3.toString.toDouble,
           r._4.toString.toDouble,
           r._5.toString.toDouble,
-          r._6.toString.toInt,
+          r._6.toString.toDouble,
           r._7.toString.toDouble,
-          r._8.toString.toInt,
+          r._8.toString.toDouble,
           r._9.toString.toDouble,
-          r._10.toString.toInt,
+          r._10.toString.toDouble,
           r._11.toString.toDouble,
           r._12.toString.toDouble,
           r._13.toString.toDouble)
@@ -107,19 +109,93 @@ object Main {
             StructField("tempo", DoubleType, false) ::
             StructField("danceability", DoubleType, false) ::
             StructField("duration", DoubleType, false) ::
-            StructField("mode", IntegerType, false) ::
+            StructField("mode", DoubleType, false) ::
             StructField("start_of_fade_out", DoubleType, false) ::
-            StructField("key", IntegerType, false) ::
+            StructField("key", DoubleType, false) ::
             StructField("energy", DoubleType, false) ::
-            StructField("time_signature", IntegerType, false) ::
+            StructField("time_signature", DoubleType, false) ::
             StructField("end_of_fade_in", DoubleType, false) ::
             StructField("analysis_sample_rate", DoubleType, false) ::
             StructField("song_hotttnesss", DoubleType, false) :: Nil)
 
-    sqlContext.createDataFrame(songsRDD, schema).show(5)
+    val songsDF = sqlContext.createDataFrame(songsRDD, schema)
 
-    
+    /*
+    //Normalize
+    val featuresRDD = songsDF.rdd.map{row =>
+//      (row.get(0), row.get(1), row.get(2), row.get(3), row.get(4), row.get(5),
+//      row.get(6), row.get(7), row.get(8), row.get(9), row.get(10), row.get(11), row.get(12))
+      (row.toSeq.head, row.toSeq.tail)
+    }
+
+    val schema2 = StructType(
+      StructField("label", DoubleType, false) ::
+        StructField("fields", ArrayType(StringType), false) :: Nil)
+
+
+    val featuresRDDforNormalization =
+      featuresRDD.map(r => {
+      Row(
+        r._1.toString.toDouble,
+        r._2.map(item => item.toString))
+    })
+
+    val featuresDFforNormalization = sqlContext.createDataFrame(featuresRDDforNormalization, schema2)
+
+    featuresDFforNormalization.take(5).foreach(println)
+
+    val arr2Vect = new Array2Vector()
+    val vectorTr = arr2Vect
+      .setInputCol("fields")
+      .setOutputCol("features")
+
+    val vectorDF = vectorTr.transform(featuresDFforNormalization)
+    vectorDF.cache()
+
+    val scaler = new StandardScaler()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+      .setWithStd(true)
+      .setWithMean(true)
+
+    // Compute summary statistics by fitting the StandardScaler
+    val scalerModel = scaler.fit(vectorDF)
+
+    // Normalize each feature to have unit standard deviation.
+    val scaledData = scalerModel.transform(vectorDF)
+    scaledData.take(5).foreach(println)
+    val normalizer = new Normalizer()
+      .setInputCol("features")
+      .setOutputCol("normFeatures")
+      .setP(2.0)
+
+    val l1NormData = normalizer.transform(vectorDF)
+    l1NormData.take(5).foreach(println)
+    */
+
+    val songDFforPipeline = songsDF.rdd.map{row =>
+      row.toSeq.foldRight(z = "")((z, a1) => if (a1.nonEmpty) z+","+a1 else z+"")
+    }
+
+    songDFforPipeline.take(5).foreach(println)
+
+    val splits = songDFforPipeline.toDF("row").randomSplit(Array(0.7, 0.3))
+
+    val obsDF = splits(0)
+    val testDF = splits(1)
+
+    val pipeline = PipelineBuilder.build(obsDF)
+
+    val pipelineModel: PipelineModel = pipeline.fit(obsDF)
+    val lReg = pipelineModel.stages(7).asInstanceOf[MyLinearModelImpl]
+    lReg.trainingError.foreach(e => {
+      println("RMSE => " + e)
+    })
+
+    lReg.transform(pipelineModel.transform(testDF).select("row", "features")).show(5)
   }
+
+
 
   def open(filename: String): Try[IHDF5Reader] = Try{HDF5FactoryProvider.get().openForReading(new File(filename))}
 
